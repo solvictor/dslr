ae1853f35bac60584688fca3636d9573c970f946
bac0nb0yy <kyuma@pm.me>
2024-11-21 22:52:39 +0100

    Automatic save if `save_folder` different than default

--- histogram.py
+++ histogram.py
@@ -10,6 +10,7 @@ DEFAULT_LOCATION_HISTOGRAM_IMAGES = "histograms"
 DEFAULT_LOCATION_FILE_DATASET = "data/dataset_train.csv"
 MOST_HOMOGENOUS_FEATURE = "Arithmancy"
 
+
 if __name__ == "__main__":
     parser = ArgumentParser(
         prog="histogram",
@@ -35,7 +36,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--show",
         action="store_true",
-        help=("Show all the plots."),
+        help=("Show all the histograms plots."),
     )
 
     parser.add_argument(
@@ -50,6 +51,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    if args.save_folder != DEFAULT_LOCATION_HISTOGRAM_IMAGES:
+        args.save = True
+
     try:
         data = pd.read_csv(args.path)
         df = data.dropna()
@@ -60,12 +64,19 @@ if __name__ == "__main__":
 
             if args.save or args.show or course == MOST_HOMOGENOUS_FEATURE:
                 plt.figure("Histogram", figsize=(10, 6))
+
                 sns.histplot(
                     data=df,
                     x=course,
                     hue="Hogwarts House",
                     multiple="stack",
                     stat="frequency",
+                    palette={
+                        "Gryffindor": "#7F0909",
+                        "Slytherin": "#1A472A",
+                        "Hufflepuff": "#FFDB00",
+                        "Ravenclaw": "#0E1A40",
+                    },
                 )
 
                 plt.title(f"Histogram of {course} Scores by Hogwarts House")
@@ -73,11 +84,9 @@ if __name__ == "__main__":
                 plt.ylabel("Frequency")
 
                 if args.save:
-                    if not os.path.exists(DEFAULT_LOCATION_HISTOGRAM_IMAGES):
-                        os.makedirs(DEFAULT_LOCATION_HISTOGRAM_IMAGES)
-                    plt.savefig(
-                        f"{DEFAULT_LOCATION_HISTOGRAM_IMAGES}/histplot_{course.lower()}.png"
-                    )
+                    if not os.path.exists(args.save_folder):
+                        os.makedirs(args.save_folder)
+                    plt.savefig(f"{args.save_folder}/histplot_{course.lower()}.png")
                 if args.show or course == MOST_HOMOGENOUS_FEATURE:
                     plt.show()
                 else:
